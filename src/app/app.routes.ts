dcc9caf554e54d751e439ee0126255708fb5a126
Nicolas1055 <riscon396@gmail.com>
2025-06-15 13:28:22 -0500

    Landing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,11 @@ import { Routes } from '@angular/router';
 
 
 // Asegúrate de que apunte a login.component.ts
-import { LoginComponent  } from './login/login.component';
 import { LandingPage  } from './landing-page/landing-page.component';
 
 export const routes: Routes = [
   { path: '', component: LandingPage},
-  { path: 'login', component: LoginComponent}, // <--- Revisa la coma al final y que 'component' apunte a LoginComponent
+  { path: 'landing-page', component: LandingPage}, // <--- Revisa la coma al final y que 'component' apunte a LoginComponent
   
   { path: '**', redirectTo: '' } // Esto es opcional, redirecciona a /login por defecto
   // Si tienes otras rutas, asegúrate de que cada objeto de ruta termine en coma (excepto el último)
